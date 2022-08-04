e347982b272d6d132f76d35eb5b31c428402039a
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-04 14:26:22 -0400

    update spec_helper with new lib files

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,11 +2,13 @@ require 'date'
 
 class Enigma
   # def initialize
-  
+  #   @date = Date.today.strftime("%d%m%Y")
+  #   @key = (Array.new(5) { rand(0..9) }).join
+  #   # could store keys in a hash
   # end
 
   def encrypt(message, key, date)
-
+  
   end
 
   def a_key(key)
@@ -23,5 +25,41 @@ class Enigma
 
   def d_key(key)
     key[3..4]
+  end
+
+  def a_offset(date)
+    date_squared = date.to_i ** 2
+    date_squared.to_s[-4]
+  end
+
+  def b_offset(date)
+    date_squared = date.to_i ** 2
+    date_squared.to_s[-3]
+  end
+
+  def c_offset(date)
+    date_squared = date.to_i ** 2
+    date_squared.to_s[-2]
+  end
+
+  def d_offset(date)
+    date_squared = date.to_i ** 2
+    date_squared.to_s[-1]
+  end
+
+  def a_final(key, date)
+    a_key(key).to_i + a_offset(date).to_i
+  end
+
+  def b_final(key, date)
+    b_key(key).to_i + b_offset(date).to_i
+  end
+
+  def c_final(key, date)
+    c_key(key).to_i + c_offset(date).to_i
+  end
+
+  def d_final(key, date)
+    d_key(key).to_i + d_offset(date).to_i
   end
 end
