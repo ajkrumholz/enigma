629f20c98b6c4801016effbe34d4430ca823f535
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-04 17:40:52 -0400

    fully test #encrypt and #decrypt with alternate casing and punctuation

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,20 +6,72 @@ RSpec.describe Enigma do
   let(:key) { "02715" }
   let(:date) { "040895" }
   
-
   it 'exists' do
     expect(enigma).to be_a(described_class)
   end
 
-  it '#encrypt' do
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
+  context 'encryption and decryption' do
+    it '#encrypt' do
+      expected = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+      
+      expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+    end
+    
+    it '#decrypt' do
+      expected = {
+        decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      }
 
-    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+      expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+    end
+
+    it '#encrypt with inconsistent casing' do
+      expected = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+      
+      expect(enigma.encrypt("HeLlo wOrld", "02715", "040895")).to eq(expected)
+    end
+
+    it '#decrypt with inconsistent casing' do
+      expected = {
+        decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      }
+
+      expect(enigma.decrypt("kEdeR Ohulw", "02715", "040895")).to eq(expected)
+    end
+
+    it '#encrypt with inconsistent casing and punctuation' do
+      expected = {
+        encryption: "keder ohulw!!",
+        key: "02715",
+        date: "040895"
+      }
+      
+      expect(enigma.encrypt("HeLlo wOrld!!", "02715", "040895")).to eq(expected)
+    end
+
+    it '#decrypt with inconsistent casing and punctuation' do
+      expected = {
+        decryption: "he!!llo world",
+        key: "02715",
+        date: "040895"
+      }
+
+      expect(enigma.decrypt("kE!!deR Ohulw", "02715", "040895")).to eq(expected)
+    end
   end
+
   context 'key creation' do 
 
     it '#keygen' do
@@ -85,15 +137,4 @@ RSpec.describe Enigma do
       expect(enigma.generate_final_offsets(key, date)).to eq(expected)
     end
   end
-
-  it '#decrypt' do
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-    }
-
-    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
-  end
-  
 end
