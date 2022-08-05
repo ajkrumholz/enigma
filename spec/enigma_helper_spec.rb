c549a124dc6e09e67b90cde98b1d6ffd4d03a082
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-05 07:32:48 -0400

    refactor testing to slim down helper methods using hashes

--- spec/enigma_helper_spec.rb
+++ spec/enigma_helper_spec.rb
@@ -16,61 +16,24 @@ RSpec.describe Cryptable do
       expect(enigma.dategen).to eq(Date.today.strftime("%d%m%Y"))  
     end
 
-    it '#a_key' do
-      expect(enigma.a_key(key)).to eq('02')
-    end
-
-    it '#b_key' do
-      expect(enigma.b_key(key)).to eq('27')
-      
-    end
-
-    it '#c_key' do
-      expect(enigma.c_key(key)).to eq('71')
-      
-    end
-
-    it '#d_key' do
-      expect(enigma.d_key(key)).to eq('15')
-      
+    it '#key_hash' do
+      expect(enigma.key_hash(key)[:a]).to eq('02')
+      expect(enigma.key_hash(key)[:b]).to eq('27')
+      expect(enigma.key_hash(key)[:c]).to eq('71')
+      expect(enigma.key_hash(key)[:d]).to eq('15')
     end
   end
 
   context 'offset creation' do
-    it '#a_offset' do
-      expect(enigma.a_offset(date)).to eq("1")
-    end
-
-    it '#b_offset' do
-      expect(enigma.b_offset(date)).to eq("0")
-    end
-
-    it '#c_offset' do
-      expect(enigma.c_offset(date)).to eq("2")
-    end
-
-    it '#d_offset' do
-      expect(enigma.d_offset(date)).to eq("5")
+    it '#offset_hash' do
+      expect(enigma.offset_hash(date)[:a]).to eq("1")
+      expect(enigma.offset_hash(date)[:b]).to eq("0")
+      expect(enigma.offset_hash(date)[:c]).to eq("2")
+      expect(enigma.offset_hash(date)[:d]).to eq("5")
     end
   end
 
   context 'final offset creation' do
-    it '#final_a_offset' do
-      expect(enigma.a_final(key, date)).to eq(3)
-    end
-
-    it '#final_b_offset' do
-      expect(enigma.b_final(key, date)).to eq(27)
-    end
-
-    it '#final_c_offset' do
-      expect(enigma.c_final(key, date)).to eq(73)
-    end
-
-    it '#final_d_offset' do
-      expect(enigma.d_final(key, date)).to eq(20)
-    end
-
     it '#generate_final_offsets' do
       expected = [3, 27, 73, 20]
       expect(enigma.generate_final_offsets(key, date)).to eq(expected)
