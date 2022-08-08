1a971e964421a4a9e6896e113f3caea6f07d62d7
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-08 15:39:03 -0400

    add methods to close all files after reading/writing

--- decrypt.rb
+++ decrypt.rb
@@ -5,11 +5,13 @@ enigma = Enigma.new
 read_file = File.open(ARGV[0], 'r')
 
 encrypted_message = read_file.read { |f| }
+read_file.close
 
 decryption_hash = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
 decryption = decryption_hash[:decryption]
 
 write_file = File.open(ARGV[1], 'w')
 write_file.write(decryption)
+write_file.close
 
 puts "created #{ARGV[1]} with the key #{ARGV[2]} and date #{ARGV[3]}"
