04a620fb58cd46663990349c6a786aa04dfe7784
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-09 09:32:56 -0400

    refactor CLI runner files to reflect new structure - organization complete

--- decrypt.rb
+++ decrypt.rb
@@ -1,17 +1,15 @@
-require_relative 'lib/enigma'
+require_relative 'lib/decryptor'
 
-enigma = Enigma.new
+decryptor = Decryptor.new
 
 read_file = File.open(ARGV[0], 'r')
-
 encrypted_message = read_file.read { |f| }
 read_file.close
 
-decryption_hash = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
-decryption = decryption_hash[:decryption]
+decryption = decryptor.decrypt(encrypted_message, ARGV[2], ARGV[3])
 
 write_file = File.open(ARGV[1], 'w')
-write_file.write(decryption)
+write_file.write(decryption[:decryption])
 write_file.close
 
-puts "created #{ARGV[1]} with the key #{ARGV[2]} and date #{ARGV[3]}"
+puts "created #{ARGV[1]} with the key #{decryption[:key]} and date #{decryption[:date]}"
