11a560e1df1a263cf77e06cf822de2806c8a56a5
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-09 10:03:46 -0400

    add context blocks to enhance readibility of tests

--- spec/decryptor_spec.rb
+++ spec/decryptor_spec.rb
@@ -5,37 +5,41 @@ require_relative 'spec_helper'
 RSpec.describe Decryptor do
   let(:decryptor) { described_class.new }
 
-  it 'exists' do
-    expect(decryptor).to be_a(described_class)
+  context 'setup and basic test' do
+    it 'exists' do
+      expect(decryptor).to be_a(described_class)
+    end
+
+    it '#decrypt' do
+      expected = {
+        decryption: 'hello world',
+        key: '02715',
+        date: '040895'
+      }
+
+      expect(decryptor.decrypt('keder ohulw', '02715', '040895')).to eq(expected)
+    end
   end
 
-  it '#decrypt' do
-    expected = {
-      decryption: 'hello world',
-      key: '02715',
-      date: '040895'
-    }
+  context 'inconsistent casing and punctuation' do
+    it '#decrypt with inconsistent casing' do
+      expected = {
+        decryption: 'hello world',
+        key: '02715',
+        date: '040895'
+      }
 
-    expect(decryptor.decrypt('keder ohulw', '02715', '040895')).to eq(expected)
-  end
+      expect(decryptor.decrypt('kEdeR Ohulw', '02715', '040895')).to eq(expected)
+    end
 
-  it '#decrypt with inconsistent casing' do
-    expected = {
-      decryption: 'hello world',
-      key: '02715',
-      date: '040895'
-    }
+    it '#decrypt with inconsistent casing and punctuation' do
+      expected = {
+        decryption: 'he!!llo world',
+        key: '02715',
+        date: '040895'
+      }
 
-    expect(decryptor.decrypt('kEdeR Ohulw', '02715', '040895')).to eq(expected)
-  end
-
-  it '#decrypt with inconsistent casing and punctuation' do
-    expected = {
-      decryption: 'he!!llo world',
-      key: '02715',
-      date: '040895'
-    }
-
-    expect(decryptor.decrypt('kE!!deR Ohulw', '02715', '040895')).to eq(expected)
+      expect(decryptor.decrypt('kE!!deR Ohulw', '02715', '040895')).to eq(expected)
+    end
   end
 end
--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -6,37 +6,41 @@ RSpec.describe Encryptor do
   let(:enigma) { Enigma.new }
   let(:encryptor) { described_class.new }
 
-  it 'exists' do
-    expect(encryptor).to be_a(described_class)
+  context 'setup and basic testing' do
+    it 'exists' do
+      expect(encryptor).to be_a(described_class)
+    end
+
+    it '#encrypt' do
+      expected = {
+        encryption: 'keder ohulw',
+        key: '02715',
+        date: '040895'
+      }
+
+      expect(encryptor.encrypt('hello world', '02715', '040895')).to eq(expected)
+    end
   end
 
-  it '#encrypt' do
-    expected = {
-      encryption: 'keder ohulw',
-      key: '02715',
-      date: '040895'
-    }
+  context 'inconsistent casing and punctuation' do
+    it '#encrypt with inconsistent casing and punctuation' do
+      expected = {
+        encryption: 'keder ohulw!!',
+        key: '02715',
+        date: '040895'
+      }
 
-    expect(encryptor.encrypt('hello world', '02715', '040895')).to eq(expected)
-  end
+      expect(encryptor.encrypt('HeLlo wOrld!!', '02715', '040895')).to eq(expected)
+    end
 
-  it '#encrypt with inconsistent casing and punctuation' do
-    expected = {
-      encryption: 'keder ohulw!!',
-      key: '02715',
-      date: '040895'
-    }
+    it '#encrypt with inconsistent casing' do
+      expected = {
+        encryption: 'keder ohulw',
+        key: '02715',
+        date: '040895'
+      }
 
-    expect(encryptor.encrypt('HeLlo wOrld!!', '02715', '040895')).to eq(expected)
-  end
-
-  it '#encrypt with inconsistent casing' do
-    expected = {
-      encryption: 'keder ohulw',
-      key: '02715',
-      date: '040895'
-    }
-
-    expect(encryptor.encrypt('HeLlo wOrld', '02715', '040895')).to eq(expected)
+      expect(encryptor.encrypt('HeLlo wOrld', '02715', '040895')).to eq(expected)
+    end
   end
 end
