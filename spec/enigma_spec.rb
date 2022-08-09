ea4a3317937a515f149e56d57740f8c1feb2e44e
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-09 08:55:41 -0400

    update enigma_spec for reorg and test #keygen using stub

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,145 +9,37 @@ RSpec.describe Enigma do
   it 'exists' do
     expect(enigma).to be_a(described_class)
   end
-
-  context 'encryption' do
-    it '#encrypt' do
-      expected = {
-        encryption: 'keder ohulw',
-        key: '02715',
-        date: '040895'
-      }
-
-      expect(enigma.encrypt('hello world', '02715', '040895')).to eq(expected)
-    end
-
-    it '#encrypt with inconsistent casing and punctuation' do
-      expected = {
-        encryption: 'keder ohulw!!',
-        key: '02715',
-        date: '040895'
-      }
-
-      expect(enigma.encrypt('HeLlo wOrld!!', '02715', '040895')).to eq(expected)
-    end
-
-    it '#encrypt with inconsistent casing' do
-      expected = {
-        encryption: 'keder ohulw',
-        key: '02715',
-        date: '040895'
-      }
-
-      expect(enigma.encrypt('HeLlo wOrld', '02715', '040895')).to eq(expected)
-    end
+  
+  it '#keygen' do
+    allow(enigma).to receive(:keygen).and_return("12345")
+    expect(enigma.keygen).to eq("12345")
   end
 
-  context 'decryption' do
-    it '#decrypt' do
-      expected = {
-        decryption: 'hello world',
-        key: '02715',
-        date: '040895'
-      }
-
-      expect(enigma.decrypt('keder ohulw', '02715', '040895')).to eq(expected)
-    end
-
-    it '#decrypt with inconsistent casing' do
-      expected = {
-        decryption: 'hello world',
-        key: '02715',
-        date: '040895'
-      }
-
-      expect(enigma.decrypt('kEdeR Ohulw', '02715', '040895')).to eq(expected)
-    end
-
-    it '#decrypt with inconsistent casing and punctuation' do
-      expected = {
-        decryption: 'he!!llo world',
-        key: '02715',
-        date: '040895'
-      }
-
-      expect(enigma.decrypt('kE!!deR Ohulw', '02715', '040895')).to eq(expected)
-    end
+  it '#character_set' do
+    expect(enigma.character_set).to eq(('a'..'z').to_a << ' ')
   end
 
-  context 'cracking the code' do
-    it '#encrypt testing' do
-      expected = {
-        encryption: 'vjqtbeaweqihssi',
-        date: '291018',
-        key: '08304'
-      }
-      expect(enigma.encrypt('hello world end', '08304', '291018')).to eq(expected)
-    end
+  it '#today_date' do
+    expect(enigma.today_date).to eq(Date.today.strftime('%d%m%y'))
+  end
 
-    it '#crack with provided date' do
-      expected = {
-        decryption: 'hello world end',
-        date: '291018',
-        key: '08304'
-      }
+  it '#key_array' do
+    expect(enigma.key_array(key)[0]).to eq(2)
+    expect(enigma.key_array(key)[1]).to eq(27)
+    expect(enigma.key_array(key)[2]).to eq(71)
+    expect(enigma.key_array(key)[3]).to eq(15)
+  end
 
-      expect(enigma.crack('vjqtbeaweqihssi', '291018')).to eq(expected)
-    end
+  it '#offset_hash' do
+    expect(enigma.offset_array(date)[0]).to eq(1)
+    expect(enigma.offset_array(date)[1]).to eq(0)
+    expect(enigma.offset_array(date)[2]).to eq(2)
+    expect(enigma.offset_array(date)[3]).to eq(5)
+  end
 
-    it '#crack with a different provided date' do
-      expected = {
-        decryption: 'hello world end',
-        date: '020385',
-        key: '33030'
-      }
-
-      expect(enigma.crack('vjqtbeaweqihssi', '020385')).to eq(expected)
-    end
-
-    it '#crack with a different message' do
-      enigma.encrypt('oh hello world end', '62774', '291018')
-
-      expected = {
-        decryption: 'oh hello world end',
-        date: '291018',
-        key: '62774'
-      }
-
-      expect(enigma.crack('bkyesojlnzmozgybag', '291018')).to eq(expected)
-    end
-
-    it "#crack with today's date" do
-      current_encrypt = enigma.encrypt('hello world end')
-
-      expected = {
-        decryption: 'hello world end',
-        date: enigma.dategen,
-        key: current_encrypt[:key]
-      }
-
-      expect(enigma.crack(current_encrypt[:encryption])[:decryption]).to eq(expected[:decryption])
-    end
-
-    it "#crack with today's date and a different message" do
-      current_encrypt = enigma.encrypt('hola world end')
-      expected = {
-        decryption: 'hola world end',
-        date: enigma.dategen,
-        key: current_encrypt[:key]
-      }
-      
-      expect(enigma.crack(current_encrypt[:encryption])[:decryption]).to eq(expected[:decryption])
-    end
-
-    it "#crack with today's date and a special characters" do
-      current_encrypt = enigma.encrypt("h0l@ worlds !!!*** end")
-      expected = {
-        decryption: "h0l@ worlds !!!*** end",
-        date: enigma.dategen,
-        key: current_encrypt[:key]
-      }
-
-      expect(enigma.crack(current_encrypt[:encryption])[:decryption]).to eq(expected[:decryption])
-    end
+  it '#generate_final_offsets' do
+    expected = [3, 27, 73, 20]
+    expect(enigma.generate_final_offsets(key, date)).to eq(expected)
   end
 end
+  
